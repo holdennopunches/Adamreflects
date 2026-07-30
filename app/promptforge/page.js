4db951e1a8abb16f0346c4763cf6129ddb0bc2d4
holdennopunches <holdennopunches@gmail.com>
2026-07-30 16:01:09 +0100

    Create page.js

---
+++ app/promptforge/page.js
@@ -0,0 +1,11 @@
+'use client';
+
+import PromptForge from '@/app/components/PromptForge';
+
+export default function PromptForgePage() {
+  return (
+    <div style={{ minHeight: '100vh', background: '#1E2030' }}>
+      <PromptForge />
+    </div>
+  );
+}
